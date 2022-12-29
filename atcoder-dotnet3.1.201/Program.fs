af84fb21c253ffc1a9555ace59047aadb248172f
phasetr <phasetr@gmail.com>
2022-12-29 21:42:35 +0900

    update

---
+++ atcoder-dotnet3.1.201/Program.fs
@@ -0,0 +1,8 @@
+﻿let solve N Aa =
+  let flip (a,b) = (b,a)
+  let Xm = Aa |> Array.distinct |> Array.sort |> Array.indexed |> Array.map flip |> Map
+  Aa |> Array.map (fun x -> Xm.[x]+1)
+
+let N = stdin.ReadLine() |> int
+let Aa = stdin.ReadLine().Split() |> Array.map int
+solve N Aa |> Array.map string |> String.concat " " |> stdout.WriteLine
